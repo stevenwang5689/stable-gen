583d467019caa9fdca25ccae49a4a7750a6b4da8
Steven Wang <zjsteven5689@gmail.com>
2020-03-04 14:47:08 -0600

    successfully call api and receive response

--- src/components/compute.js
+++ src/components/compute.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Button from '@material-ui/core/Button';
 import axios from 'axios';
 
@@ -6,7 +6,8 @@ class Compute extends Component {
   constructor(props) {
     super(props);
       this.state = {
-        selectedFile: null
+        selectedFile: null,
+        polymers: []
       }
   }
 
@@ -21,19 +22,50 @@ class Compute extends Component {
     reader.onload = async (e) => { 
       const text = (e.target.result)
       console.log(text)
-      axios.post("http://localhost:5005", text,{
+      axios.post("http://localhost:5005/", text,{
         headers: {
           'Content-Type' : 'application/json'
         }
       })
-      .then(function (response) {
-        console.log(response);
-      })
+      .then((data) => {
+        var jsonResponse = JSON.parse(data.request.response)
+
+        this.setState({
+          polymers: jsonResponse.configs
+        })
+    })
     };
     reader.readAsText(this.state.selectedFile);
+    
   }
 
+  
+
   render() {
+    var i = 0
+    var listItems = this.state.polymers.map((poly) => {
+      var listOfPolymers = poly.polymers.map((polymers) => {
+        var monomers = polymers.map(monomer => {
+          return(
+            <p> {monomer} </p>
+          )
+        });
+        return(
+          <Fragment>
+          <p>{monomers}</p>
+          <br/>
+          </Fragment>
+      )})
+      return( <Fragment>
+            <h1>Configuration {++i}</h1>
+            <p>This configuration has {poly.polymers_count} polymers</p>
+            <box>
+              {listOfPolymers}
+            </box>
+
+            
+        </Fragment>)
+    })
     return (
       <div>
         <br/>
@@ -46,6 +78,8 @@ class Compute extends Component {
           />
         </Button>
         <Button type="button" color="secondary" onClick={this.onClickHandler}>Compute</Button>
+
+        <h1> {listItems} </h1>
       </div>
     );
   }
