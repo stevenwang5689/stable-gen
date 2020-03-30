eb6a1dea97324515f3aeb4b73c349da200d6408c
Steven Wang <zjsteven5689@gmail.com>
2020-03-30 00:06:11 -0500

    added entropy and config count for output

--- src/components/computeComponents/output.js
+++ src/components/computeComponents/output.js
@@ -1,27 +1,14 @@
 import React, { Component, Fragment } from 'react';
 import {MContext} from "../provider";
 
+import '../../App.css';
 import Result from "./result";
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField';
-import CloudUploadIcon from '@material-ui/icons/CloudUpload';
-import SendIcon from '@material-ui/icons/Send';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Switch from '@material-ui/core/Switch';
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import GetAppIcon from '@material-ui/icons/GetApp';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import Paper from '@material-ui/core/Paper';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import axios from 'axios';
 
 class Output extends Component {
     state = {  }
@@ -30,7 +17,16 @@ class Output extends Component {
             <Fragment>
                 <MContext.Consumer>
                     {(context) => (
-                        <Grid container direction="column">
+                        <Grid container spacing={3} direction="column">
+                            {(context.state.entropy !== 0) && (
+                            <Grid item>
+                                <Card>
+                                    <CardContent className="Output">
+                                        <Typography variant="h6"> Entropy: {context.state.entropy} </Typography> 
+                                        <Typography variant="h6"> Configuration Count: {context.state.count} </Typography> 
+                                    </CardContent>
+                                </Card>
+                            </Grid>)}
                             {(context.state.result.length !== 0) && ( 
                             <Grid item>
                                 <Grid container justify="flex-end">
